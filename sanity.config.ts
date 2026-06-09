7c3c7da41c0d528937aceae2434ec36cc8fc123d
Nylite-soda <oluwaseunajb@gmail.com>
2026-06-09 08:18:55 -0700

    Remove hardcoded credentials and fix studio basePath

--- sanity.config.ts
+++ sanity.config.ts
@@ -4,11 +4,24 @@ import { structureTool } from 'sanity/structure'
 import { visionTool } from '@sanity/vision'
 import { schemaTypes } from './src/sanity/schemaTypes'
 
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET!
+
 export default defineConfig({
-  name: 'dokita-eleyin',
+  name: 'default',
   title: 'Dókítà Eléyín Studio',
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET!,
-  plugins: [structureTool(), visionTool()],
-  schema: { types: schemaTypes },
+  
+  projectId,
+  dataset,
+
+  basePath: '/studio',
+
+  plugins: [
+    structureTool(),
+    visionTool(),
+  ],
+
+  schema: {
+    types: schemaTypes,
+  },
 })
